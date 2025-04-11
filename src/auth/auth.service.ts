110345088cd418736010e1b53084f5db89d08e49
Gustavo Inkotte <gus.inkotte@gmail.com>
2025-04-10 23:44:52 -0300

    fix: removed user password from query

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,19 +8,22 @@ import { Prisma } from 'generated/prisma';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
 export class AuthService {
   @Inject()
-  private readonly userService: UserService;
+  private readonly jwtService: JwtService;
 
   @Inject()
-  private readonly jwtService: JwtService;
+  private readonly prisma: PrismaService;
 
   async signin(
     params: Prisma.UserCreateInput,
   ): Promise<{ access_token: string }> {
-    const user = await this.userService.user({ email: params.email });
+    const user = await this.prisma.user.findUnique({
+      where: { email: params.email },
+    });
 
     if (!user) {
       throw new NotFoundException('User not found');
