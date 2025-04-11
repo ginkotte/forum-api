45eb03a290d2e697dd336efa364d0c4c166df0e8
Gustavo Inkotte <gus.inkotte@gmail.com>
2025-04-10 22:13:20 -0300

    feat: jwt authentication

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,10 +6,12 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 
 import { Prisma, User as UserModel } from '../../generated/prisma';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -22,16 +24,19 @@ export class UserController {
     return await this.userService.createUser(userData);
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   async getUser(@Param('id') id: string): Promise<UserModel | null> {
     return this.userService.user({ id: id });
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async deleteUser(@Param('id') id: string): Promise<UserModel> {
     return this.userService.deleteUser({ id: id });
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
   async updateUser(
     @Body() userData: Prisma.UserUpdateInput,
